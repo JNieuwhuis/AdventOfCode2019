b7deaf90dbe08fc6c73ca886a9aa2495c3320ca7
Jackolien Nieuwhuis <jackolien.nieuwhuis@quadrant.nl>
2019-12-01 21:45:50 +0100

    Solution to ass.1

--- Day1/Assignment1.cs
+++ Day1/Assignment1.cs
@@ -1,36 +1,52 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
+using System.Runtime.CompilerServices;
 
-public class Assignment1
+namespace AdventOfCode2019.Day1
 {
-	public Assignment1()
-	{
-	}
-
-    public static void Main()
+    public class Assignment1
     {
-        try
+        public Assignment1()
         {
-            using (StreamReader sr = new StreamReader("Input.txt"))
+        }
+
+        private static int totalFuelNeeded = 0;
+
+        public static void Main()
+        {
+            ReadInput();
+        }
+
+        public static void ReadInput()
+        {
+            try
             {
-                String line = sr.ReadToEnd();
-                Console.WriteLine(line);
+                using (StreamReader sr = new StreamReader(@"C:\Users\jan\Source\Repos\AdventOfCode2019\Day1\Input.txt"))
+                {
+                    while (sr.Peek() >= 0)
+                    {
+                        String line = sr.ReadLine();
+                        FuelCounterUpper(line);
+                    }
+                    
+                    Console.WriteLine("Total fuel needed: " + totalFuelNeeded);
+                    Console.ReadKey();
+                }
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("The file could not be read:");
+                Console.WriteLine(e.Message);
             }
         }
-        catch (IOException e)
+
+        public static void FuelCounterUpper(string massValue)
         {
-            Console.WriteLine("The file could not be read:");
-            Console.WriteLine(e.Message);
+            int mass = Int32.Parse(massValue);
+            int fuelNeeded = (mass / 3) - 2;
+            //Console.WriteLine("Fuel needed: " + fuelNeeded);
+            totalFuelNeeded += fuelNeeded;
         }
-    }
-
-    public void ReadInput()
-    {
-
-    }
-
-    public void FuelCounterUpper()
-    {
-
     }
 }
