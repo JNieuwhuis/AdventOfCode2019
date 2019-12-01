2ea58cbe78afb93f808841e6d480579be02a87e7
Jackolien Nieuwhuis <jackolien.nieuwhuis@quadrant.nl>
2019-12-01 20:58:44 +0100

    Test commit

---
+++ Day1/Assignment1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.IO;
+
+public class Assignment1
+{
+	public Assignment1()
+	{
+	}
+
+    public static void Main()
+    {
+        try
+        {
+            using (StreamReader sr = new StreamReader("Input.txt"))
+            {
+                String line = sr.ReadToEnd();
+                Console.WriteLine(line);
+            }
+        }
+        catch (IOException e)
+        {
+            Console.WriteLine("The file could not be read:");
+            Console.WriteLine(e.Message);
+        }
+    }
+
+    public void ReadInput()
+    {
+
+    }
+
+    public void FuelCounterUpper()
+    {
+
+    }
+}
